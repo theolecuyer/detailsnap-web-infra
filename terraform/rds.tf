20ed9d4a31c355cf86a98d56dd7b75b94bc980ca
Theo Lecuyer <theoglecuyer@gmail.com>
2026-05-03 12:28:03 -0700

    feat: add RDS and AWS secrets manager

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,1 +1,65 @@
-# Add rds (seperate schemas per env)
+resource "random_password" "db_master" {
+  length           = 24
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "detailsnap"
+  subnet_ids = module.vpc.private_subnets
+}
+
+resource "aws_security_group" "rds" {
+  name        = "detailsnap-rds"
+  description = "RDS MySQL"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "main" {
+  identifier        = "detailsnap"
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+  storage_type      = "gp2"
+
+  db_name  = "detailsnap"
+  username = "admin"
+  password = random_password.db_master.result
+
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+
+  multi_az            = false
+  skip_final_snapshot = true
+  deletion_protection = false
+}
+
+resource "aws_secretsmanager_secret" "db_credentials" {
+  name                    = "detailsnap/db"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "db_credentials" {
+  secret_id = aws_secretsmanager_secret.db_credentials.id
+  secret_string = jsonencode({
+    host     = aws_db_instance.main.address
+    port     = aws_db_instance.main.port
+    username = aws_db_instance.main.username
+    password = random_password.db_master.result
+  })
+}
