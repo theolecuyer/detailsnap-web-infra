8a4ebafd0c4f9d4a98273a5df58bcd84afd46976
Theo Lecuyer <theoglecuyer@gmail.com>
2026-05-03 12:04:13 -0700

    feat: add ACM certificate validation

---
+++ terraform/acm.tf
@@ -0,0 +1,35 @@
+data "aws_route53_zone" "main" {
+  name         = "tlecuyer.codes"
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "main" {
+  domain_name               = "tlecuyer.codes"
+  subject_alternative_names = ["*.tlecuyer.codes"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
