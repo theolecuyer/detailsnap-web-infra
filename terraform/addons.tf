0dff94a587d70afa8f00cdb2722aeda7919420f5
Theo Lecuyer <theoglecuyer@gmail.com>
2026-05-05 17:20:45 -0700

    feat: add ESO and external dns

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -74,3 +74,59 @@ resource "helm_release" "argocd" {
 
   depends_on = [helm_release.aws_lbc]
 }
+
+resource "helm_release" "external_secrets" {
+  name             = "external-secrets"
+  repository       = "https://charts.external-secrets.io"
+  chart            = "external-secrets"
+  namespace        = "external-secrets"
+  create_namespace = true
+  wait             = true
+  timeout          = 300
+
+  depends_on = [aws_eks_node_group.envs]
+}
+
+resource "null_resource" "cluster_secret_store" {
+  triggers = {
+    eso_version = helm_release.external_secrets.version
+  }
+
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${aws_eks_cluster.main.name} --region us-east-1 && kubectl apply -f ../k8s/cluster-secret-store.yaml"
+  }
+
+  depends_on = [helm_release.external_secrets]
+}
+
+resource "helm_release" "external_dns" {
+  name             = "external-dns"
+  repository       = "https://kubernetes-sigs.github.io/external-dns/"
+  chart            = "external-dns"
+  namespace        = "external-dns"
+  create_namespace = true
+  wait             = true
+  timeout          = 300
+
+  set {
+    name  = "provider.name"
+    value = "aws"
+  }
+
+  set {
+    name  = "sources[0]"
+    value = "service"
+  }
+
+  set {
+    name  = "sources[1]"
+    value = "gateway-httproute"
+  }
+
+  set {
+    name  = "txtOwnerId"
+    value = "detailsnap"
+  }
+
+  depends_on = [null_resource.gateway_api_crds, aws_eks_node_group.envs]
+}
