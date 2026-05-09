a18ddb875290ef5158a68c3b4dfb80ef93f370f9
Theo Lecuyer <theoglecuyer@gmail.com>
2026-05-09 14:52:34 -0700

    scale system node group to 2 (max 3) and restore monitoring nodeSelectors

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -89,9 +89,9 @@ resource "aws_eks_node_group" "system" {
   }
 
   scaling_config {
-    desired_size = 1
-    min_size     = 1
-    max_size     = 1
+    desired_size = 2
+    min_size     = 2
+    max_size     = 3
   }
 
   update_config {
