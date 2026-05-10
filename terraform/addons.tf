51476c1896dae8f66a8b9e3a4ef5f2498196140e
Theo Lecuyer <theoglecuyer@gmail.com>
2026-05-10 16:59:54 -0700

    fix: revert gateway-gateway source, fix RBAC values heredoc syntax

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -220,11 +220,6 @@ resource "helm_release" "external_dns" {
   }
 
   set {
-    name  = "sources[2]"
-    value = "gateway-gateway"
-  }
-
-  set {
     name  = "txtOwnerId"
     value = "detailsnap"
   }
@@ -244,18 +239,13 @@ resource "helm_release" "external_dns" {
     value = "system"
   }
 
-  values = [
-    yamlencode({
-      clusterRole = {
-        extraRules = [
-          {
-            apiGroups = ["gateway.networking.k8s.io"]
-            resources = ["gateways", "httproutes"]
-            verbs     = ["get", "list", "watch"]
-          }
-        ]
-      }
-    })
+  values = [<<-YAML
+    clusterRole:
+      extraRules:
+        - apiGroups: ["gateway.networking.k8s.io"]
+          resources: ["gateways", "httproutes"]
+          verbs: ["get", "list", "watch"]
+  YAML
   ]
 
   depends_on = [helm_release.aws_lbc, null_resource.gateway_api_crds, null_resource.wait_for_system_node]
